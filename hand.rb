917f8bb55b4a281ba221e27d4387791c90361b58
Nordveyl <kkrutoychel.wf@gmail.com>
2021-12-12 21:15:12 +0300

    Update hand.rb

--- hand.rb
+++ hand.rb
@@ -1,25 +1,8 @@
 class Hand
-  attr_reader :points, :cards 
+  attr_accessor :points, :cards 
 
-  def initialize(deck)  
-    @cards =[]
-    @points = 0 
-    @deck = deck 
+  def initialize 
+    @cards = []
+    @points = 0  
   end 
-
-  def take_card 
-    card = Card.new(@deck.values[Random.rand(0..12)], @deck.suits[Random.rand(0..3)])
-    @cards << card 
-    if card.value =~ /[2, 3, 4, 5, 6, 7, 8, 9, 10]/
-      @points += card.value.to_i
-    elsif card.value =~ /[J, Q, K]/
-      @points += 10
-    elsif card.value =~ /[A]/
-      if @points <= 10 
-        @points +=11 
-      else 
-        @points += 1
-      end    
-    end 
-  end                 
 end 
