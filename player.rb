a376d274df3d2dac81005917b9903f764bd94579
Nordveyl <kkrutoychel.wf@gmail.com>
2021-12-12 21:15:17 +0300

    Update player.rb

--- player.rb
+++ player.rb
@@ -1,17 +1,7 @@
 require_relative 'User'
 class Player < User
   
-  def initialize(deck) 
-    @@bank_of_player ||=100
+  def initialize 
     super
   end 
-
-  def first_move 
-    @@bank_of_player -= 10
-    super
-  end 
-    
-  def self.bank
-    @@bank_of_player 
-  end  
 end 
